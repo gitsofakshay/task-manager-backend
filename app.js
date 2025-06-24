d60a61e14166350f009996f3507fcd1a6a6f5d29
Akshay Raj Kushwaha <akshayrajkushwaha69660@gmail.com>
2025-06-24 22:52:19 +0530

    Add cors confiration

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const taskRoutes = require('./routes/taskRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const allowedOrigin = process.env.FRONTEND_URL || 'http://localhost:5173';
+app.use(cors({
+  origin: allowedOrigin,
+  credentials: true,
+}));
 app.use(express.json());
 
 // Routes
